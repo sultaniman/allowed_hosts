29871e2cb7aea00af569f5342bbca5ada35678b7
Sultan Imanhodjaev <sultan.imanhodjaev@gmail.com>
2016-12-04 19:55:09 +0100

    update(plug) add more default matches & re-indent file

--- lib/allowed_hosts.ex
+++ lib/allowed_hosts.ex
@@ -1,14 +1,18 @@
 defmodule AllowedHosts do
-	@allow_all ["*"]
+	import Plug.Conn, only: [send_resp: 3]
 
-	def init([]), do: @allow_all
-	def init(options), do: options
+  @allow_all ["*"]
 
-	def call(conn, hosts) when hosts |> is_list do
-		host = conn.get_header("host")
-		
-		unless Enum.member?(hosts, hosts) do
-			# TODO
-		end
-	end
+  def init([]), do: @allow_all
+  def init(nil), do: @allow_all
+  def init(options), do: options
+
+  def call(conn, nil), do: conn
+  def call(conn, @allow_all), do: conn
+
+  def call(conn, hosts) when hosts |> is_list do
+    unless Enum.member?(hosts, conn.host) do
+      conn |> send_resp(400, "Bad request")
+    end
+  end
 end
