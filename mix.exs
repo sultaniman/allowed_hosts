2bbe0d9b80e3fd21c2b153a6ddcd85dfe45ffaa5
Sultan Iman <sultan.imanhodjaev@gmail.com>
2020-02-05 12:56:13 +0100

    Cleanup mixfile and update deps

--- mix.exs
+++ mix.exs
@@ -1,19 +1,40 @@
 defmodule AllowedHosts.Mixfile do
   use Mix.Project
 
+  @vsn "0.1.0"
+
+  @package [
+    files: ~w(lib mix.exs README.md LICENSE CHANGELOG.md),
+    maintainers: ["Sultan Iman"],
+    licenses: ["MIT"],
+    links: %{
+      "Github" => "http://github.com/imanhodjaev/allowed_hosts",
+      "Docs"   => "http://hexdocs.pm/allowed_hosts"
+    }
+  ]
+
+  @description "Elixir plug to protect your APIs from Host header attacks."
+
+  @deps [
+    {:cowboy, "~> 2.7"},
+    {:plug, "~> 1.8.0"},
+
+    # Dev deps
+    {:ex_doc, "~> 0.21.3", only: :dev},
+    {:earmark, "~> 1.4", only: :dev}
+  ]
+
   def project do
     [
       app: :allowed_hosts,
-      version: "0.1.0",
-      elixir: "~> 1.3",
+      version: @vsn,
+      elixir: "~> 1.8",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
-      package: package,
-      description: description,
-      docs: [
-        extras: ~W(README.md CHANGELOG.md)
-      ],
-      deps: deps()
+      package: @package,
+      description: @description,
+      docs: [extras: ~W(README.md CHANGELOG.md)],
+      deps: @deps
     ]
   end
 
@@ -22,43 +43,5 @@ defmodule AllowedHosts.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
-  end
-
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
-  defp deps do
-    [
-      {:cowboy, "~> 1.0.0"},
-      {:plug, "~>1.3.0"},
-
-      # Dev deps
-      {:ex_doc, "~> 0.11", only: :dev},
-      {:earmark, "~> 1.0.3", only: :dev},
-    ]
-  end
-
-  defp description do
-    """
-    Elixir plug to protect your APIs from Host header attacks.
-    """
-  end
-
-  defp package do
-    [
-      files: ~w(lib mix.exs README.md LICENSE CHANGELOG.md),
-      maintainers: ["Sultan Iman"],
-      licenses: ["MIT"],
-      links: %{
-        "Github" => "http://github.com/imanhodjaev/allowed_hosts",
-        "Docs"   => "http://hexdocs.pm/allowed_hosts",
-      }
-    ]
   end
 end
